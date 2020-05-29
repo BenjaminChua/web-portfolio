b92004faabca2f1cf808cd0fac544ce953e1a173
benjaminchua <boyz_pro95@hotmail.com>
2020-05-29 18:30:30 +0800

    Updates

---
+++ precache-manifest.fa56ef71e4e7966d6ddb3dd58d526b17.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa9d7ed7eb3f8867781741bcc35e272",
+    "url": "/web-portfolio/index.html"
+  },
+  {
+    "revision": "101f5d5e6c0609736ede",
+    "url": "/web-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d22e7ceccf8159e5ab2b",
+    "url": "/web-portfolio/static/css/main.86281725.chunk.css"
+  },
+  {
+    "revision": "101f5d5e6c0609736ede",
+    "url": "/web-portfolio/static/js/2.c7e1238f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/web-portfolio/static/js/2.c7e1238f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22e7ceccf8159e5ab2b",
+    "url": "/web-portfolio/static/js/main.677efc40.chunk.js"
+  },
+  {
+    "revision": "5f2bd7db5f3db5961ddb",
+    "url": "/web-portfolio/static/js/runtime-main.1ae9af0b.js"
+  },
+  {
+    "revision": "b6db986d098a12a8ec06a856644990dd",
+    "url": "/web-portfolio/static/media/AISG.b6db986d.jpg"
+  },
+  {
+    "revision": "c4ba85eb08527c807abeb275f86be4f1",
+    "url": "/web-portfolio/static/media/Experian.c4ba85eb.jpg"
+  },
+  {
+    "revision": "08403f5a5980c7c85888d7f0c0192d0e",
+    "url": "/web-portfolio/static/media/GT.08403f5a.jpg"
+  },
+  {
+    "revision": "399a94aab51bdb0af65aa57ce17fc083",
+    "url": "/web-portfolio/static/media/KPMG.399a94aa.jpg"
+  },
+  {
+    "revision": "e782f9ac5b78642cebc14605ba42c20a",
+    "url": "/web-portfolio/static/media/NUS.e782f9ac.jpg"
+  },
+  {
+    "revision": "568a58b5bc52ecef03031df42e223e94",
+    "url": "/web-portfolio/static/media/Udemy.568a58b5.jpg"
+  },
+  {
+    "revision": "296acfa7af59bb3a2a5da255843a73bd",
+    "url": "/web-portfolio/static/media/educationCloud.296acfa7.jpg"
+  },
+  {
+    "revision": "b2c7b6b154c61479b178c286b18de728",
+    "url": "/web-portfolio/static/media/exchange.b2c7b6b1.jpg"
+  },
+  {
+    "revision": "07b676c13a22cb7e5df1500972bb9c0d",
+    "url": "/web-portfolio/static/media/exchangeBBall.07b676c1.jpg"
+  },
+  {
+    "revision": "2440e586b1b44d5624c6094f722ca9d0",
+    "url": "/web-portfolio/static/media/exchangeClouds.2440e586.jpg"
+  },
+  {
+    "revision": "40567cf5c4def1e884c1b2fe611904be",
+    "url": "/web-portfolio/static/media/exchangeDC.40567cf5.jpg"
+  },
+  {
+    "revision": "659166d4226d4a128ecce4ddf05023f1",
+    "url": "/web-portfolio/static/media/exchangeDisney.659166d4.jpg"
+  },
+  {
+    "revision": "172225562e770c8e4c6a8b5948dd115d",
+    "url": "/web-portfolio/static/media/exchangeGT.17222556.jpg"
+  },
+  {
+    "revision": "12a3751a200480b4a4a92449f20b13fa",
+    "url": "/web-portfolio/static/media/exchangeHouston.12a3751a.jpg"
+  },
+  {
+    "revision": "431ba0bca29687cb9a08821a770aefab",
+    "url": "/web-portfolio/static/media/exchangeMIT.431ba0bc.jpg"
+  },
+  {
+    "revision": "616ebc676e831a2749e84073caac1d43",
+    "url": "/web-portfolio/static/media/exchangePresChurch.616ebc67.PNG"
+  },
+  {
+    "revision": "04c173885f70ea6d20d27f50863ab397",
+    "url": "/web-portfolio/static/media/exchangeSFsea.04c17388.jpg"
+  },
+  {
+    "revision": "c7db6c1ec15b624ca73dcbc913160bab",
+    "url": "/web-portfolio/static/media/exchangeSeattle.c7db6c1e.jpg"
+  },
+  {
+    "revision": "cb92a869c9f78090f17cff0b7d901671",
+    "url": "/web-portfolio/static/media/exchangeSend.cb92a869.jpg"
+  },
+  {
+    "revision": "a49a6332e32274d4680fefff63b8d185",
+    "url": "/web-portfolio/static/media/experienceCloud.a49a6332.jpg"
+  },
+  {
+    "revision": "5e16a4a863a1bef62828bd4f47020fd8",
+    "url": "/web-portfolio/static/media/selfPortrait.5e16a4a8.jpg"
+  }
+]);
